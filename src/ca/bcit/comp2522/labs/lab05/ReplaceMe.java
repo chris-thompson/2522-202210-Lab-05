9c354de15217d77ece318d81427d68b901ed5510
Chris <chris_thompson@bcit.ca>
2022-02-06 22:23:23 -0800

    Hello world!

---
+++ src/ca/bcit/comp2522/labs/lab05/ReplaceMe.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.labs.lab05;
+
+public class ReplaceMe {
+
+    public static void main(String[] args) {
+        System.out.println("Replace this class. Go ahead. I'm just here to make the package.");
+    }
+}
